5827ad4f86038639bac74bec30d44e0284f0c2dd
Sean Coh <scoh21@basecampcodingacademy.org>
2020-11-09 14:20:03 -0600

    class account setup

--- Save_Better_Bank.py
+++ Save_Better_Bank.py
@@ -6,21 +6,27 @@ main_menu = input("""What would you like to do?
     - Create an account
     - Delete an existing account
     Type Here: """
-)
 
 class users:
     def __init__(self, name, password):
         self.name = name
         self.password = password
 
-balance = 0
 class account:
-    def __init__(self, balance):
-        self.balance = balance
+    def __init__(self):
+        self.balance = 0
     
     def deposit(self, deposit):
-        self.deposit = self.deposit + balance
+        self.balance = self.balance + deposit
+        return self.balance
     
     def withdraw(self, withdraw):
-        if self.balance >= amount:
-            self.withdraw - balance
+        if self.balance >= withdraw:
+            self.balance - withdraw
+    
+    def total_balance(self):
+        return self.balance
+
+# user = account()
+# user1 = user.withdraw(10)
+# print(f'Your balance is ${user.balance:.2f}')

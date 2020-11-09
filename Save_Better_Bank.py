9b7eeca4dbea2ab34dc1a00a82cce19d0d9744c8
Ethan Bishop <ethancbishop1@gmail.com>
2020-11-09 14:19:11 -0600

    Class Users Setup, Create Account Function.

--- Save_Better_Bank.py
+++ Save_Better_Bank.py
@@ -1,17 +1,44 @@
 import sqlite3
 import Bank_Logic
-print("Welcome to SaveBetterBank(SBB)")
-main_menu = input("""What would you like to do?
-    - Login
-    - Create an account
-    - Delete an existing account
-    Type Here: """
-)
 
 class users:
     def __init__(self, name, password):
         self.name = name
         self.password = password
+
+    def login_info(self):
+        return self.name, self.password
+    
+
+def create_account():
+    user_name = input("Please provide a username you would like to use(Ex: ethan_bishop.20) - ")
+    user_password = input("Type a strong Password: ")
+    completed_user = users(user_name, user_password)
+    print(f"Your User Name is [{completed_user.name}], Your password is [{completed_user.password}]")
+    print("Account Has Been Created!")
+
+
+print("Welcome to SaveBetterBank(SBB)")
+main_menu = input("""What would you like to do?(Type Letter of what you want to do.)
+    - a) Login
+    - b) Create an account
+    - c) Delete an existing account
+    Type Here: """
+)
+
+while True:
+    if main_menu == "a":
+        login()
+        break
+    elif main_menu == "b":
+        create_account()
+        break
+    elif main_menu == "c":
+        remove_account()
+        break
+    else:
+        
+
 
 balance = 0
 class account:

dc4ecd85981614968ad1146117be6c409d546dfc
Sean Coh <scoh21@basecampcodingacademy.org>
2020-11-09 13:29:11 -0600

    class account created

--- Save_Better_Bank.py
+++ Save_Better_Bank.py
@@ -5,5 +5,14 @@ class users:
         self.name = name
         self.password = password
 
-
-
+balance = 0
+class account:
+    def __init__(self, balance):
+        self.balance = balance
+    
+    def deposit(self, deposit):
+        self.deposit = self.deposit + balance
+    
+    def withdraw(self, withdraw):
+        if self.balance >= amount:
+            self.withdraw - balance
